813a122bee268bc9d7fad5d563f8537052323d05
MamunCh0wdhury <189634349+asrhospitals@users.noreply.github.com>
2025-07-22 09:36:28 +0530

    changes in main.js Db url changes

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 const { Sequelize } = require("sequelize");
 
 /// For Dev
-// const sequelize = new Sequelize('lims_database', 'postgres', 'Postgres123', {
-//    host: 'localhost',
-//    dialect: 'postgres',
-//    port: 5432,
-//  });
+const sequelize = new Sequelize('lims_database', 'postgres', 'Postgres123', {
+   host: 'localhost',
+   dialect: 'postgres',
+   port: 5432,
+ });
 
 
 
 
-///For Production
-const sequelize = new Sequelize(process.env.DB_URL, {
-  dialect: "postgres",
-  port: 5432,
-});
+// ///For Production
+// const sequelize = new Sequelize(process.env.DB_URL, {
+//   dialect: "postgres",
+//   port: 5432,
+// });
 
 module.exports = sequelize;
